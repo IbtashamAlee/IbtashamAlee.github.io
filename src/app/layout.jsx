5a468cb1e30f8b3c670483ca3e09b0a7342ecbfc
ibtasham.gsoftconsulting <ibtasham@gsoftconsulting.com>
2023-11-06 22:51:05 +0500

    added seo

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,9 +6,9 @@ import '@/styles/tailwind.css'
 
 export const metadata = {
   title: {
-    template: '%s - Ibtasham Ali',
+    template: '%s | Ibtasham Ali',
     default:
-      'Ibtasham Ali - Passionate Software Engineer, Tech Enthusiast, and Fitness Advocate',
+      'Ibtasham Ali - Software Engineer',
   },
   description:
     'I am Ibtasham Ali, Software Engineer specialized in JavaScript, particularly with a focus on Node.js. With extensive experience in crafting exceptional software solutions, I bring a strong skill set to every project. Beyond my love for technology, I am also an avid gym-goer, committed to maintaining a strong and healthy physique.',
@@ -17,9 +17,34 @@ export const metadata = {
     title: "Ibtasham Ali - Passionate Software Engineer, Tech Enthusiast, and Fitness Advocate",
     description:
     'I am Ibtasham Ali, a Software Engineer specializing in JavaScript, particularly with an emphasis on Node.js. With a wealth of experience in developing exceptional software solutions, I bring a robust skill set to every project. Apart from my enthusiasm for technology, I am dedicated to regular gym sessions, prioritizing the maintenance of a strong and healthy physique.',
-    images: "https://firebasestorage.googleapis.com/v0/b/better-cui.appspot.com/o/IMG_0713.jpg?alt=media&token=e7f13f3a-ba7b-4474-b25f-ab879b8128b6&_gl=1*gqzm95*_ga*MTgxMTg0NzMzMC4xNjk5Mjg1ODI3*_ga_CW55HF8NVT*MTY5OTI4NTgyNy4xLjEuMTY5OTI4NjA1My42MC4wLjA.",
+    images: "https://firebasestorage.googleapis.com/v0/b/better-cui.appspot.com/o/IMG_0713-%20squared.jpg?alt=media&token=0091bd53-9288-4f9b-b0d2-bb9b56df8023&_gl=1*uw1azc*_ga*MTgxMTg0NzMzMC4xNjk5Mjg1ODI3*_ga_CW55HF8NVT*MTY5OTI4ODAyOC4yLjEuMTY5OTI4ODA4OC42MC4wLjA.",
     url: `${process.env.NEXT_PUBLIC_SITE_URL}`,
   },
+  icons: {
+    shortcut: "https://firebasestorage.googleapis.com/v0/b/better-cui.appspot.com/o/IMG_0713-%20squaredx64.jpg?alt=media&token=7a371124-097b-4267-9862-ffea17f78c34&_gl=1*liqejs*_ga*MTgxMTg0NzMzMC4xNjk5Mjg1ODI3*_ga_CW55HF8NVT*MTY5OTI4ODAyOC4yLjEuMTY5OTI4ODA2MC4yOC4wLjA."
+  },
+  keywords: [
+    "Ibtasham Ali",
+    "Ibtasham Alee",
+    "Ibtashamalee",
+    "ibtashamalee.com",
+    "alee jaan",
+    "Software Engineer",
+    "Programming",
+    "Coding",
+    "Fitness",
+    "Gym",
+    "Science",
+    "Computer Science",
+    "Management",
+    "Leadership",
+    "Business",
+    "Software Development",
+    "Germany",
+    "Portfolio",
+    "Ibtasham Ali Portfolio",
+    "Ibtasham Alee Portfolio",
+  ],  
   robots: {
     index: true,
     follow: true
@@ -34,6 +59,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <head>
+        <link rel="shortcut icon" href="https://firebasestorage.googleapis.com/v0/b/better-cui.appspot.com/o/IMG_0713-%20squaredx64.jpg?alt=media&token=7a371124-097b-4267-9862-ffea17f78c34&_gl=1*liqejs*_ga*MTgxMTg0NzMzMC4xNjk5Mjg1ODI3*_ga_CW55HF8NVT*MTY5OTI4ODAyOC4yLjEuMTY5OTI4ODA2MC4yOC4wLjA." />
+      </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
