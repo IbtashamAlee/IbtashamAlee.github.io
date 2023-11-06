671ec1a8508ac31725d6231ae7632331250a3edb
ibtasham.gsoftconsulting <ibtasham@gsoftconsulting.com>
2023-11-06 19:31:05 +0500

    fix config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 const withMDX = require('@next/mdx')({
   extension: /\.mdx?$/,
-})
+});
 
-module.exports = withMDX({
+const nextConfig = {
+  experimental: {
+    runtime: 'experimental-edge',
+  },
+  reactStrictMode: true,
+  swcMinify: true,
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-})
+};
+
+module.exports = withMDX(nextConfig);
+
+
+
+// const withMDX = require('@next/mdx')({
+//   extension: /\.mdx?$/,
+// })
+
+// module.exports = withMDX({
+//   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+// })
