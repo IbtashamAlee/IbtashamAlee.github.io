492dcfb345bd4465780caa54be2c41384e520e0e
ibtasham.gsoftconsulting <ibtasham@gsoftconsulting.com>
2023-11-06 19:51:11 +0500

    feed fix

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
-const withMDX = require('@next/mdx')({
-  extension: /\.mdx?$/,
-});
-
-const nextConfig = {
-  runtime: 'edge', // for Edge API Routes only
-  unstable_allowDynamic: [
-    // allows a single file
-    '/lib/utilities.js',
-    // use a glob to allow anything in the function-bind 3rd party module
-    '/node_modules/function-bind/**',
-  ],
-  reactStrictMode: true,
-  swcMinify: true,
-  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-};
-
-module.exports = withMDX(nextConfig);
-
-
-
 // const withMDX = require('@next/mdx')({
 //   extension: /\.mdx?$/,
-// })
+// });
 
-// module.exports = withMDX({
+// const nextConfig = {
+//   runtime: 'edge', // for Edge API Routes only
+//   unstable_allowDynamic: [
+//     // allows a single file
+//     '/lib/utilities.js',
+//     // use a glob to allow anything in the function-bind 3rd party module
+//     '/node_modules/function-bind/**',
+//   ],
+//   reactStrictMode: true,
+//   swcMinify: true,
 //   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-// })
+// };
+
+// module.exports = withMDX(nextConfig);
+
+
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+})
+
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
+})
