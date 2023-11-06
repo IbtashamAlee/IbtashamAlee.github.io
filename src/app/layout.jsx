2a424a92bcd5fbd58fa1393d2e34e1dc08898fb0
ibtasham.gsoftconsulting <ibtasham@gsoftconsulting.com>
2023-11-06 05:05:09 +0500

    seo and small fix

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,7 +1,10 @@
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
+import AboutImage from '@/images/portrait.jpg'
 
 import '@/styles/tailwind.css'
+
+export const runtime = 'edge' // 'nodejs' (default) | 'edge'
 
 export const metadata = {
   title: {
@@ -11,6 +14,18 @@ export const metadata = {
   },
   description:
     'I am Ibtasham Ali, Software Engineer specialized in JavaScript, particularly with a focus on Node.js. With extensive experience in crafting exceptional software solutions, I bring a strong skill set to every project. Beyond my love for technology, I am also an avid gym-goer, committed to maintaining a strong and healthy physique.',
+  openGraph: {
+    title: "About - Ibtasham Ali",
+    description:
+    'I am Ibtasham Ali, a Software Engineer specializing in JavaScript, particularly with an emphasis on Node.js. With a wealth of experience in developing exceptional software solutions, I bring a robust skill set to every project. Apart from my enthusiasm for technology, I am dedicated to regular gym sessions, prioritizing the maintenance of a strong and healthy physique.',
+    images: [
+      {
+        url: AboutImage,
+        alt: 'Ibtasham Ali - Software Engineer',
+      },
+    ],
+    url: `${process.env.NEXT_PUBLIC_SITE_URL}/about`,
+  },
   alternates: {
     types: {
       'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
